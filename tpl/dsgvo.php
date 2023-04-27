837c47fd10000c742a3485cef34e789b762882c2
gizmore <gizmore@wechall.net>
2023-04-27 21:10:19 +0200

    '7.0.3-r1783r4'

--- tpl/dsgvo.php
+++ tpl/dsgvo.php
@@ -5,6 +5,6 @@ namespace GDO\DSGVO\tpl;
     <div class="gdo_dsgvo_overlay"></div>
     <div class="gdo_dsgvo_info">
         <p><?=t('dsgvo_info')?></p>
-        <div><a id="gdo-dsgvo-accept-button" href="<?=href('DSGVO', 'Accept', '&_ref=' . (@$_SERVER['REQUEST_URI']))?>"><?=t('btn_accept')?></a></div>
+        <div><a id="gdo-dsgvo-accept-button" href="<?=href('DSGVO', 'Accept', '&_ref=' . urlencode(@$_SERVER['REQUEST_URI']))?>"><?=t('btn_accept')?></a></div>
     </div>
 </div>
